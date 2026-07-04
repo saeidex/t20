762c4450c3e9f58b725f384d2f25bba7d10deb8d
EK Nayeem <ebrahim.khalil.nayeem@gmail.com>
2026-07-04 21:46:33 +0600

    chore(build): fix build by skip node_modules bundling

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/cli/main.ts"],
+  entry: {
+    bin: "src/cli/main.ts",
+  },
   clean: true,
+  minify: true,
   publicDir: true,
   treeshake: "smallest",
   bundle: true,
   format: "cjs",
   platform: "node",
+  skipNodeModulesBundle: true,
 });
